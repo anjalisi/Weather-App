cfd073c2d5e314ea511ce156a947a168c3f978aa
Anjali Singh <muskan.anjali.singh@gmail.com>
2020-06-30 22:10:51 +0530

    Constructing the views

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void findWeather(View view)
+    {
+        
+    }
+    public class DownloadJSON extends AsyncTask<String, Void, String>
+    {
+        @Override
+        protected String doInBackground(String... urls)
+        {
+            String result= "";
+            URL url;
+            HttpURLConnection connection= null;
+            try
+            {
+                url= new URL(urls[0]);
+                connection= (HttpURLConnection)url.openConnection();
+                InputStream is= connection.getInputStream();
+                InputStreamReader reader= new InputStreamReader(is);
+                int data= reader.read();
+
+                while(data != -1)
+                {
+                    char current= (char)data;
+                    result += current;
+                    data= reader.read();
+                }
+                return result;
+            }
+
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //Step 1: Converting String to actual JSON Data
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                //Step 2: Extracting the favourable part
+                String weatherinfo = jsonObject.getString("weather");    //Extracts the weather part
+                Log.i("Weather Content", weatherinfo);
+
+                //Step 3: Looping through the data
+                JSONArray jsonArray = new JSONArray(weatherinfo);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonPart = jsonArray.getJSONObject(i);
+                    Log.i("Main", jsonPart.getString("main"));
+                    Log.i("Description", jsonPart.getString("description"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
